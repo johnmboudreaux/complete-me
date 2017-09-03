e151cbf53b4a4599dd3c2c689fc15efbaecfb90c
johnmboudreaux <jhnbdrx@gmail.com>
2017-09-03 13:57:07 -0600

    rewrote insert func

--- tests/completMe-test.js
+++ tests/completMe-test.js
@@ -1,17 +1,47 @@
 const { assert, expect } = require ('chai');
 const CompleteMe = require ('../lib/completeMe.js');
-// const text = "/usr/share/dict/words"
-// const fs = require('fs')
-// const dictionary = fs.readFileSync(text).toString().trim().split('\n')
+const Branch = require('../lib/branch');
+// const App = require('../lib/app.js');
+let completion;
+const text = "/usr/share/dict/words"
+const fs = require('fs')
+const dictionary = fs.readFileSync(text).toString().trim().split('\n')
 
 
 describe('CompleteMe', () => {
 
-  it('should be a function', () => {
-    let completeMe = new CompleteMe();
+  beforeEach ( ()=> {
+    completion = new CompleteMe
+  })
 
-    assert.isFunction(completeMe);
+  it('should be a object', () => {
 
+    assert.isObject(completion);
   });
+
+  it('should instantiate a new branch', () => {
+    let branch = new Branch();
+
+    assert.instanceOf(branch, Branch, 'heeeelp')
+  })
+
+  it('should have insert function', () => {
+    //should have a function that will insert a new word as a string
+    // let branch = new Branch()
+
+    expect(completion.insert).to.be.a('function');
+
+    // completion.populate(['apply', 'apple', 'bananas', 'cat', 'frog']);
+    completion.insert('anyword');
+    completion.insert('anywhere');
+
+    // expect(completion.words.length).to.equal(235886);
+    // expect(completion.words[0]).to.equal(dictionary)
+  })
+
+  // it('should have count function', () => {
+  //   //should have a function that will keep count of what we are doing
+  //   expect(completion.count).to.be.a('function');
+  // })
 
 });
--- tests/index-test.js
+++
@@ -1,5 +0,0 @@
-import { expect } from 'chai';
-import index from '../scripts/index'
-
-describe('Example Test File', () => {
-})
